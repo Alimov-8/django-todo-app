83f91ba636b1100aeadfa0b0a159d7807151b605
Abdullokh Alimov <66109159+Alimov-8@users.noreply.github.com>
2020-11-22 23:44:25 +0500

    Update wsgi.py

--- ToDoProject/ToDoProject/wsgi.py
+++ ToDoProject/ToDoProject/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for ToDoProject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+path = os.path.expanduser('~/ToDoProject')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ToDoProject.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ToDoProject.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
